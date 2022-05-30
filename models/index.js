b9eaf1eaf661906cd5591735c6b8ff06875a8754
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-30 13:46:22 -0500

    models complete

--- models/index.js
+++ models/index.js
@@ -1,2 +1,29 @@
 const User = require("./USER");
 const Post = require("./POST");
+const Comment = require("./COMMENT");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+User.hasMany(Comment, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Post.hasMany(Comment, {
+  foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: "post_id",
+});
+
+module.exports = { User, Post, Comment };
